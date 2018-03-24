a9215d4675ea37a70ecbbb940d0ee1a970bc17cf
Dan Waltin <dan.waltin@agero.se>
2018-03-24 16:05:40 +0100

    Added a health status to the service

---
+++ CircuitBreaker/Controllers/ServiceStatusController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using FinanceService;
+
+namespace CircuitBreaker.Controllers
+{
+	public class ServiceHealthController : ApiController
+	{
+		[HttpGet]
+		public ServiceHealth ShowServiceHealth()
+		{
+			return ZooService.Health;
+		}
+
+		[HttpPost]
+		public void SetServiceHealth(ServiceHealth health)
+		{
+			ZooService.Health = health;
+		}
+	}
+}
--- Services/ZooService.cs
+++ Services/ZooService.cs
@@ -1,17 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Dto;
 
 namespace FinanceService
 {
-    public class ZooService : IZooService
-    {
-	    public IEnumerable<Animal> ListAnimals()
-	    {
-		    return new List<Animal>
-		    {
-			    new Animal { Species = "Dog", Name = "Fido" },
-			    new Animal { Species = "Cat", Name = "Pelle" }
-		    };
+	public enum ServiceHealth
+	{
+		Alive,
+		Slow,
+		Crashed
+	}
+
+	public class ZooService : IZooService
+	{
+		public static ServiceHealth Health = ServiceHealth.Alive;
+
+		public IEnumerable<Animal> ListAnimals()
+		{
+			CheckHealth();
+
+			return new List<Animal> {
+				new Animal { Species = "Dog", Name = "Fido" },
+				new Animal { Species = "Cat", Name = "Pelle" }
+			};
+		}
+
+		private void CheckHealth()
+		{
+			switch (Health)
+			{
+				case ServiceHealth.Alive:
+					break;
+				case ServiceHealth.Slow:
+					Thread.Sleep(30 * 1000);
+					break;
+				case ServiceHealth.Crashed:
+					throw new NotImplementedException("Oops!");
+				default:
+					throw new ArgumentException($"Status '{Health}' not allowed");
+			}
 		}
 	}
 }
